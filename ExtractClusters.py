8508b221cbea222425487e751f3ad06eeb9b8364
Ryan A. Melnyk <schmelnyk@gmail.com>
2016-10-03 18:23:37 -0700

    align and generate HMMs for group summaries

--- ExtractClusters.py
+++ ExtractClusters.py
@@ -4,6 +4,7 @@
 
 import argparse, os, re
 from Bio import SeqIO
+import subprocess
 
 def parse_args():
 	parser = argparse.ArgumentParser(description='''
@@ -29,6 +30,7 @@ def hash_fastas(outdir):
 def parse_groups(seqdata, desc, outdir):
 	group_count = 1
 	descript_out = open(os.path.join(outdir, "group_descriptions.txt"),'w')
+	print "Writing fasta files and parsind descriptions..."
 	# by default choose clustering where inflation == 2.0
 	for line in open(os.path.join(outdir,"mcl","clusters.2.txt")):
 		vals = line.rstrip().split()
@@ -37,27 +39,46 @@ def parse_groups(seqdata, desc, outdir):
 		for v in vals:
 			o.write(">{}\n{}\n".format(v,seqdata[v]))
 			this_descript.append(desc[v])
-		descript_out.write("\t".join(this_descript)+"\n")
+		descript_out.write("group_{}\t{}\n".format(str(group_count).zfill(5),"\t".join(this_descript)))
 		o.close()
 		group_count += 1
 
 	return
 
 def setupdir(outdir):
-	for f in ["homolog_fasta"]:
+	for f in ["homolog_fasta","aligned","hmms"]:
 		try:
 			os.makedirs(os.path.join(os.path.join(outdir,f)))
 		except OSError:
 			print "Subfolder exists:", outdir
 	return
 
+def align_groups(outdir):
+	print "Aligning groups..."
+	for f in os.listdir(os.path.join(outdir, "homolog_fasta")):
+		cmds = "muscle -in {} -out {}".format(os.path.join(outdir,"homolog_fasta",f),os.path.join(outdir,"aligned",f.split(".")[0]+'.aln'))
+		proc = subprocess.Popen(cmds.split())
+		proc.wait()
+	return
+
+def build_hmms(outdir):
+	print "Building hmms..."
+	for f in os.listdir(os.path.join(outdir, "aligned")):
+		cmds = "hmmbuild {} {}".format(os.path.join(outdir,"hmms",f.split(".")[0]+".hmm"),os.path.join(outdir,"aligned",f))
+		proc = subprocess.Popen(cmds.split())
+		proc.wait()
+	return
+
 def main():
 	args = parse_args()
 	outdir = os.path.abspath(args.outdir)
+	path_to_pfam = os.path.abspath(args.path_to_pfam)
 
 	setupdir(outdir)
 	seqdata, desc = hash_fastas(outdir)
 	parse_groups(seqdata, desc, outdir)
+	align_groups(outdir)
+	build_hmms(outdir)
 
 if __name__ == '__main__':
 	main()
