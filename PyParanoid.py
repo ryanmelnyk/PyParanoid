35a1712f781b080283a6f32532c61002af84b09d
Ryan A. Melnyk <schmelnyk@gmail.com>
2017-07-11 15:10:54 -0700

    started to develop parallelization mode for PyParanoid

--- PyParanoid.py
+++ PyParanoid.py
@@ -17,7 +17,9 @@ Note that default diamond uses 6 threads when requesting resources.
 	parser.add_argument('genomedb',type=str,help='relative path to genomedb for raw data')
 	parser.add_argument('strainlist',type=str,help='text file, one strain per line. names should be "species" field from genome_metadata table')
 	parser.add_argument('outdir',type=str,help='folder to store output')
-	parser.add_argument('--add',action='store_true',help='use if you intend to add to an existing PyParanoid output folder')
+	parser.add_argument('--cpus',type=int,help='number of CPUs to use for diamond tasks (default = 6)')
+	parser.add_argument('--mode',type=str,help='mode of PyParanoid to run')
+	parser.add_argument('--njobs',type=int,help='use to specify number of batch files to make (default=10)')
 	return parser.parse_args()
 
 def setupdir(outdir,strains,genomedb):
@@ -64,56 +66,76 @@ def make_diamond_databases(strains,outdir):
 			pass
 	return
 
-def run_diamond(strains,outdir):
-	print "Running reflexive diamond on all", len(strains), "strains..."
+def run_diamond(strains,outdir,multi,cpus):
+	if multi:
+		jobfile = open(os.path.join(outdir,"diamond_jobs.sh"),'w')
+		print "Making jobfile for DIAMOND runs..."
+	else:
+		print "Running reflexive DIAMOND on all", len(strains), "strains..."
+
 	count = len(strains)
 	for s in strains:
-		cmds = "diamond blastp --query {}/faa/{}.faa -d {}/dmnd/{}.dmnd -o {}/m8/{}.{}.m8 -f tab --min-score 50 --quiet --threads 6".format(outdir,s,outdir,s,outdir,s,s)
-		proc = subprocess.Popen(cmds.split())
-		proc.wait()
-		count -= 1
-		if count == 0:
-			print "\tDone!"
-		elif (count % 10 == 0):
-			print "\t"+str(count), "remaining..."
+		cmds = "diamond blastp --query {}/faa/{}.faa -d {}/dmnd/{}.dmnd -o {}/m8/{}.{}.m8 -f tab --min-score 50 --quiet --threads {}".format(outdir,s,outdir,s,outdir,s,s,cpus)
+		if multi:
+			jobfile.write("{}\n".format(cmds))
 		else:
-			pass
+			proc = subprocess.Popen(cmds.split())
+			proc.wait()
+			count -= 1
+			if count == 0:
+				print "\tDone!"
+			elif (count % 10 == 0):
+				print "\t"+str(count), "remaining..."
+			else:
+				pass
 
 	pairs = list(itertools.permutations(strains,2))
 	count = len(pairs)
-	print "Running pairwise diamond on all", count, "pairwise permutations..."
+	if not multi:
+		print "Running pairwise diamond on all", count, "pairwise permutations..."
 	for p in list(pairs):
-		cmds = "diamond blastp --query {}/faa/{}.faa -d {}/dmnd/{}.dmnd -o {}/m8/{}.{}.m8 -f tab --min-score 50 --quiet --threads 6".format(outdir,p[0],outdir,p[1],outdir,p[0],p[1])
-		proc = subprocess.Popen(cmds.split())
-		proc.wait()
-		count -= 1
-		if count == 0:
-			print "\tDone!"
-		elif count % 10 == 0:
-			print "\t"+str(count), "remaining..."
+		cmds = "diamond blastp --query {}/faa/{}.faa -d {}/dmnd/{}.dmnd -o {}/m8/{}.{}.m8 -f tab --min-score 50 --quiet --threads {}".format(outdir,p[0],outdir,p[1],outdir,p[0],p[1],cpus)
+		if multi:
+			jobfile.write("{}\n".format(cmds))
 		else:
-			pass
-	return
-
-def run_diamond_on_old_strains(strains,outdir,old_strains):
-
-	count = 2 * len(strains) * len(old_strains)
-	print "Running pairwise diamond against all old strains:", count, "pairwise permutations ..."
-	for s in strains:
-		for t in old_strains:
-			cmds = "diamond blastp --query {}/faa/{}.faa -d {}/dmnd/{}.dmnd -o {}/m8/{}.{}.m8 -f tab --min-score 50 --quiet --threads 6".format(outdir,s,outdir,t,outdir,s,t)
 			proc = subprocess.Popen(cmds.split())
 			proc.wait()
-			cmds = "diamond blastp --query {}/faa/{}.faa -d {}/dmnd/{}.dmnd -o {}/m8/{}.{}.m8 -f tab --min-score 50 --quiet --threads 6".format(outdir,t,outdir,s,outdir,t,s)
-			proc = subprocess.Popen(cmds.split())
-			proc.wait()
-			count -= 2
+			count -= 1
 			if count == 0:
 				print "\tDone!"
 			elif count % 10 == 0:
 				print "\t"+str(count), "remaining..."
 			else:
 				pass
+	return
+
+def run_diamond_on_old_strains(strains,outdir,old_strains,multi,cpus):
+	if multi:
+		jobfile = open(os.path.join("outdir","diamond_jobs.sh"),'a')
+	else:
+		count = 2 * len(strains) * len(old_strains)
+		print "Running pairwise diamond against all old strains:", count, "pairwise permutations ..."
+	for s in strains:
+		for t in old_strains:
+			cmds = "diamond blastp --query {}/faa/{}.faa -d {}/dmnd/{}.dmnd -o {}/m8/{}.{}.m8 -f tab --min-score 50 --quiet --threads {}".format(outdir,s,outdir,t,outdir,s,t,cpus)
+			if multi:
+				jobfile.write("{}\n".format(cmds))
+			else:
+				proc = subprocess.Popen(cmds.split())
+				proc.wait()
+			cmds = "diamond blastp --query {}/faa/{}.faa -d {}/dmnd/{}.dmnd -o {}/m8/{}.{}.m8 -f tab --min-score 50 --quiet --threads {}".format(outdir,t,outdir,s,outdir,t,s,cpus)
+			if multi:
+				jobfile.write("{}\n".format(cmds))
+			else:
+				proc = subprocess.Popen(cmds.split())
+				proc.wait()
+				count -= 2
+				if count == 0:
+					print "\tDone!"
+				elif count % 10 == 0:
+					print "\t"+str(count), "remaining..."
+				else:
+					pass
 	return
 
 
@@ -215,27 +237,45 @@ def main():
 	outdir = os.path.abspath(args.outdir)
 	pypath = os.path.abspath(os.path.dirname(sys.argv[0]))
 
+	if args.mode:
+		if args.mode not in ["add","multi_setup","multi_parse","multi_paranoid"]:
+			print "Unknown mode!!! Exiting..."
+			sys.exit()
 
-	if args.add:
-		print "adding"
+	if args.njobs:
+		njobs = args.njobs
+	else:
+		njobs = 10
+
+	if args.cpus:
+		cpus = args.cpus
+	else:
+		cpus = 6
+
+	if args.mode == "add":
 		setupdir(outdir,strains,genomedb)
 		old_strains = [x.rstrip() for x in open(os.path.join(outdir,"strainlist.txt"),'r')]
 		check_unique(old_strains,strains)
 		append_strains(outdir,os.path.abspath(args.strainlist))
 		make_diamond_databases(strains,outdir)
-		run_diamond(strains,outdir)
-		run_diamond_on_old_strains(strains,outdir,old_strains)
+		run_diamond(strains,outdir,False,cpus)
+		run_diamond_on_old_strains(strains,outdir,old_strains,False,cpus)
 		[strains.append(s) for s in old_strains]
 		genes = get_genes(strains,outdir)
 		parse_diamond(genes,outdir)
-		run_inparanoid(strains, outdir,pypath)
+		run_inparanoid(strains,outdir,pypath)
 		clean_up(outdir)
+	elif args.mode == "multi_setup":
+		pass
+	elif args.mode == "multi_parse":
+		pass
+	elif args.mode == "multi_paranoid":
+		pass
 	else:
-		print "not adding"
 		setupdir(outdir,strains,genomedb)
 		shutil.copy(os.path.abspath(args.strainlist),os.path.join(outdir,"strainlist.txt"))
 		make_diamond_databases(strains,outdir)
-		run_diamond(strains,outdir)
+		run_diamond(strains,outdir,False,cpus)
 		genes = get_genes(strains,outdir)
 		parse_diamond(genes,outdir)
 		run_inparanoid(strains, outdir,pypath)
