586e985ab56bbcd133859a9cfc94d562805c268d
Ryan A. Melnyk <schmelnyk@gmail.com>
2017-11-02 12:45:09 -0700

    removed hmm files for annotation

--- setup.py
+++ setup.py
@@ -10,10 +10,11 @@ scripts = ['AddLocalGenome.py', 'ClusterStats.py','DownloadEnsemblGenomes.py', '
 	'IdentifyOrthologs.py','PropagateGroups.py','PyParanoid.py'],
 version='0.1dev',
 description='Fast and scalable homolog detection for thousands of genomes',
-long_description=open('README.md').read()
+long_description=open('README.md').read(),
 author='Ryan A. Melnyk',
 author_email='schmelnyk@gmail.com',
 license='MIT',
 url='https://github.com/ryanmelnyk/PyParanoid',
 install_requires=['biopython', 'numpy', 'ncbi_genome_download', 'pandas', 'seaborn', 'matplotlib','reportlab'],
+classifiers=['Programming Language :: Python','Programming Language :: Python :: 2']
 )
