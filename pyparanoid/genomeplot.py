cdf9588ed77fc7fe0cbd9554daf6e07df9d568f0
Ryan A. Melnyk <schmelnyk@gmail.com>
2017-11-16 18:07:02 -0800

    Added methods to make the HomologPresenceTree

--- pyparanoid/genomeplot.py
+++ pyparanoid/genomeplot.py
@@ -3,9 +3,10 @@ from Bio.Graphics import GenomeDiagram
 from Bio.SeqFeature import FeatureLocation
 import seaborn as sns
 import random
-import argparse, os, sys
-import seaborn as sns
+import argparse, os, sys, math
 from reportlab.lib.units import inch
+from ete3 import Tree, TreeStyle, NodeStyle
+import matplotlib.colors as colors
 
 def parse_genbank(g):
 	if g[2] == "ensembl" or g[2] == "refseq":
@@ -110,3 +111,92 @@ def make_tracks(seq, span, coords, g, GD, count, locus_tags, labels):
 						pass
 
 	return
+
+
+def match_seqs(fastafile,outdir):
+	os.system("phmmer --tblout {} {} {}".format(os.path.join(os.path.abspath(outdir),".phmmer.hits"),os.path.abspath(fastafile),os.path.join(outdir,"all_groups.faa")))
+	hit_scores = {}
+	for line in open(os.path.join(outdir,".phmmer.hits"),'r'):
+		if line.startswith("#"):
+			continue
+		else:
+			vals = line.rstrip().split()
+			hit_scores[vals[0]] = float(vals[5])
+	hit_gene_lengths = {}
+	for seq in SeqIO.parse(open(os.path.join(outdir,"all_groups.faa"),'r'),'fasta'):
+		if seq.id in hit_scores:
+			hit_gene_lengths[seq.id] = len(seq.seq)
+	norm_scores = {}
+	for group in hit_scores:
+		norm_scores[group] = hit_scores[group]/float(hit_gene_lengths[group])
+	for i in sorted(norm_scores.iteritems(), key=lambda (k,v): (v,k), reverse=True):
+		if i[1] > 1.0:
+			score = "Good match!"
+		if i[1] > 0.5 and i[1] < 1.0:
+			score = "Possible match..."
+		if i[1] < 0.5:
+			score = "Probably not a very good match..."
+		print i[0],round(i[1],3), score
+	return
+
+def add_group_to_tree(group,treefile,outdir,to_compress=False):
+	if to_compress:
+		compress = to_compress.split(",")
+	else:
+		compress = []
+	for line in open(os.path.join(outdir,"homolog_matrix.txt"),'r'):
+		if line.startswith("\t"):
+			header = line.rstrip().split("\t")[1:]
+		if not line.startswith(group):
+			continue
+		else:
+			vals = [int(x) for x in line.rstrip().split("\t")[1:]]
+	groupdata = dict(zip(header,vals))
+
+	ts = TreeStyle()
+	tree = Tree(os.path.abspath(treefile))
+
+	pal = sns.cubehelix_palette(rot=-.4, n_colors=13)
+	for node in tree.iter_descendants("preorder"):
+		this_node = []
+		nstyle = NodeStyle()
+		nstyle["shape"] = "circle"
+
+		if node.is_leaf():
+			try:
+				if groupdata[node.name] > 0:
+					nstyle["fgcolor"] = colors.rgb2hex(pal[12])
+				else:
+					nstyle["fgcolor"] = colors.rgb2hex(pal[0])
+			except KeyError:
+				nstyle["fgcolor"] = colors.rgb2hex(pal[0])
+		else:
+			species = {}
+			for x in node.iter_descendants("preorder"):
+				if x.is_leaf():
+					this_node.append(x.name)
+					s = x.name.split("_")[1]
+					if s in species:
+						species[s] += 1
+					else:
+						species[s] = 1
+			for c in compress:
+				try:
+					if float(species[c])/float(len(this_node)) > 0.95:
+						nstyle["draw_descendants"] = False
+						node.name = "{} clade".format(c)
+				except KeyError:
+					pass
+			count = 0
+			for t in this_node:
+				try:
+					if groupdata[t] > 0:
+						count +=1
+				except KeyError:
+					pass
+			v = int(round(float(count)/float(len(this_node))*12))
+			nstyle["fgcolor"] = colors.rgb2hex(pal[v])
+			nstyle["size"] = 3*math.sqrt(len(this_node))
+		node.set_style(nstyle)
+
+	return tree
